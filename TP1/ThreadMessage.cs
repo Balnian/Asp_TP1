6fe54c5cdc30f1c842158c10e27c2bd6f868a269
Balnian <super_f_qc@hotmail.com>
2015-04-12 17:55:09 -0400

    Je sais pas si sa a de l'allure mais ces sa qui est sa

--- TP1/ThreadMessage.cs
+++ TP1/ThreadMessage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -18,9 +19,51 @@ namespace TP1
             SQLTableName = "THREADS_MESSAGES";
         }
 
+        public override void GetValues()
+        {
+            ID = long.Parse(FieldsValues[0]);
+            Thread_ID = long.Parse(FieldsValues[1]);
+            User_ID = long.Parse(FieldsValues[2]);
+            Date_Of_Creation = DateTime.Parse(FieldsValues[3]);
+            Message = FieldsValues[4];
+        }
+
+
+
+        public long GetId(String threadName)
+        {
+
+            String Query = "Select ID from " + SQLTableName + " Where TITLE = '" + threadName + "'";
+            SqlConnection Connection;
+            SqlDataReader Reader;
+            long iD = -1;
+            // instancier l'objet de collection
+            Connection = new SqlConnection(connexionString);
+            // bâtir l'objet de requête
+            SqlCommand sqlcmd = new SqlCommand(Query, Connection);
+            Page.Application.Lock();
+            try
+            {
+                Connection.Open();
+                Reader = sqlcmd.ExecuteReader();
+
+                while (Reader.Read())
+                    iD = Reader.GetInt64(0);
+                Reader.Close();
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                EndQuerySQL();
+            }
+            return iD;
+        }
+
         public override void Insert()
         {
-            InsertRecord(creator, title, date);
+            InsertRecord(Thread_ID, User_ID, Date_Of_Creation, Message);
         }
     }
 }
