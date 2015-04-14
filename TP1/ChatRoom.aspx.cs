bae5a763edfe7729061b62448159159b68c05e07
Balnian <super_f_qc@hotmail.com>
2015-04-13 21:21:30 -0400

    Redirect si pas connecter

--- TP1/ChatRoom.aspx.cs
+++ TP1/ChatRoom.aspx.cs
@@ -10,18 +10,23 @@ namespace TP1
     public partial class ChatRoom : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {          
-            SetMessage();
-            ShowThreadUser();
-            ShowUser();
+        {
+            if (Session["User"] != null)
+            {
+                SetMessage();
+                ShowThreadUser();
+                ShowUser();
+            }
+            else
+                Response.Redirect("Login.aspx");
         }
 
         public void SetMessage()
         {
             ThreadMessage message = new ThreadMessage((string)Application["MainDB"], this);
-            message.SelectAll();           
+            message.SelectAll();
             message.MessageGridView(Message_Panel);
-            message.EndQuerySQL();     
+            message.EndQuerySQL();
         }
         public void SetThread()
         {
@@ -29,13 +34,13 @@ namespace TP1
             if (Session["User"] != null)
             {
                 user = (Users)Session["User"];
-                user.SelectAll();             
+                user.SelectAll();
                 Threads thread = new Threads((string)Application["MainDB"], this);
-                user.EndQuerySQL();   
-                thread.SelectAll();              
+                user.EndQuerySQL();
+                thread.SelectAll();
                 thread.ShowThread(Thread_Panel, user.ID);
                 thread.EndQuerySQL();
-            }                
+            }
         }
         protected void Btn_Send_Click(object sender, EventArgs e)
         {
@@ -50,7 +55,7 @@ namespace TP1
                 Tmessage.Date_Of_Creation = DateTime.Now.ToShortTimeString();
                 Tmessage.Message = Tb_Message.Text;
                 Tmessage.Insert();
-                Tmessage.EndQuerySQL();            
+                Tmessage.EndQuerySQL();
             }
 
             SetMessage();
@@ -58,32 +63,32 @@ namespace TP1
 
         protected void Timer_Chat_Tick(object sender, EventArgs e)
         {
-       
-          
+
+
         }
         public void ShowThreadUser()
         {
 
 
-            Threads thread = new Threads((string)Application["MainDB"], this);          
+            Threads thread = new Threads((string)Application["MainDB"], this);
             thread.SelectAll();
             thread.MakeThreadList(Thread_Panel, 2);
-            thread.EndQuerySQL();                  
+            thread.EndQuerySQL();
         }
 
         public void ShowUser()
         {
             Users user;
-             if (Session["User"] != null)
+            if (Session["User"] != null)
             {
                 user = (Users)Session["User"];
                 user.SelectAll();
-                user.listAccessThread(User_Panel,2,(List<long>)Application["Online"]);
+                user.listAccessThread(User_Panel, 2, (List<long>)Application["Online"]);
                 user.EndQuerySQL();
             }
-        
-        
-        
+
+
+
         }
     }
 }
