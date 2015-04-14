860b7d3b7b73590091a88c2cf1fdf333ded20104
Balnian <super_f_qc@hotmail.com>
2015-04-14 13:00:45 -0400

    JS ThreadMode Function

--- TP1/js.js
+++ TP1/js.js
@@ -1,5 +1,6 @@
 
 
+
 function PreLoadImage(e) {
     var imageTarget = document.getElementById("IMG_Avatar");
     var input = event.target;
@@ -9,6 +10,7 @@ function PreLoadImage(e) {
         fReader.onloadend = function (event) {
             // the event.target.result contains the image data 
             imageTarget.src = event.target.result;
+
         }
     }
     return true;
@@ -19,6 +21,20 @@ function ChatMode(event) {
     var separatorPos = messageId.lastIndexOf("_");
     var Type = messageId.slice(messageId.indexOf("_")+1, separatorPos);
     var Id = messageId.slice(separatorPos + 1, messageId.length);
-   
+
+    alert("ChatRoom.aspx?Type=" + Type + "&Id=" + Id);
     window.location.href = "ChatRoom.aspx?Type=" + Type + "&Id=" + Id;
+
+
+}
+
+function ThreadMode(event) {
+    var messageId = event.getAttribute("id");
+    var separatorPos = messageId.lastIndexOf("_");
+    var Id = messageId.slice(separatorPos + 1, messageId.length);
+
+    alert("ThreadsManager.aspx?Id=" + Id);
+    window.location.href = "ThreadsManager.aspx?Id=" + Id;
+
+
 }
