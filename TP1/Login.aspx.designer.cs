62e21a99d7c3995452118848b3058ede41376f9b
Balnian <super_f_qc@hotmail.com>
2015-04-16 09:10:15 -0400

    validator && blacklist on login

--- TP1/Login.aspx.designer.cs
+++ TP1/Login.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace TP1 {
     public partial class Login {
         
         /// <summary>
+        /// Contrôle Unamegroup.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Unamegroup;
+        
+        /// <summary>
         /// Contrôle TB_UserName.
         /// </summary>
         /// <remarks>
@@ -22,6 +31,24 @@ namespace TP1 {
         protected global::System.Web.UI.WebControls.TextBox TB_UserName;
         
         /// <summary>
+        /// Contrôle FunameVal.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator FunameVal;
+        
+        /// <summary>
+        /// Contrôle pwd1group.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pwd1group;
+        
+        /// <summary>
         /// Contrôle TB_PassWord.
         /// </summary>
         /// <remarks>
@@ -29,5 +56,32 @@ namespace TP1 {
         /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TB_PassWord;
+        
+        /// <summary>
+        /// Contrôle fpwd1Val.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator fpwd1Val;
+        
+        /// <summary>
+        /// Contrôle ErrorOverview.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel ErrorOverview;
+        
+        /// <summary>
+        /// Contrôle Subscribe_Validation.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary Subscribe_Validation;
     }
 }
